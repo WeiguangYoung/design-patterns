d5aa25596c03cfedea8a8da1d62dcbcdccee2281
杨伟光 <yangweiguang@starxtec.com>
2021-08-12 10:20:58 +0800

    finish 00

---
+++ 00_simple_factory/simple.go
@@ -0,0 +1,30 @@
+package simplefactory
+
+import "fmt"
+
+// API api接口
+type API interface {
+	Say(name string) string
+}
+
+// NewAPI 根据type返回不同的api
+func NewAPI(t int) API {
+	if t == 1 {
+		return &hiAPI{}
+	} else if t == 2 {
+		return &helloAPI{}
+	}
+	return nil
+}
+
+type hiAPI struct{}
+
+func (*hiAPI) Say(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
+
+type helloAPI struct{}
+
+func (*helloAPI) Say(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
